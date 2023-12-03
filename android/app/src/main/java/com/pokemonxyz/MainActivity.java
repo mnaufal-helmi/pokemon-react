7d39d9949fcab6653aaea18fad2655ff07bc055a
mnaufalhelmi <mnaufalhelmi@gmail.com>
2023-12-03 12:51:01 +0700

    resolve error splash screen in MainActivty.Java

--- android/app/src/main/java/com/pokemonxyz/MainActivity.java
+++ android/app/src/main/java/com/pokemonxyz/MainActivity.java
@@ -1,23 +1,20 @@
 package com.pokemonxyz;
 
-import android.os.Bundle; // here
+import android.os.Bundle; 
 import com.facebook.react.ReactActivity;
-// react-native-splash-screen >= 0.3.1
-import org.devio.rn.splashscreen.SplashScreen; // here
-// react-native-splash-screen < 0.3.1
-// import com.cboy.rn.splashscreen.SplashScreen; // here
-
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
-   @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this, R.style.SplashScreenTheme);
-        super.onCreate(savedInstanceState);
-    }
+    
+     @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
