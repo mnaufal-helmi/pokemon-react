c193bd519725069436841e0aa404a50a18bd913d
mnaufalhelmi <mnaufalhelmi@gmail.com>
2023-12-04 15:12:39 +0700

    adjustment splashscreen -1 file

--- src/app/screen/splash/index.tsx
+++ src/app/screen/splash/index.tsx
@@ -1,22 +1,61 @@
-import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {View, StyleSheet, TouchableOpacity, Text} from 'react-native';
+import Video from 'react-native-video';
+import {useNavigation} from '@react-navigation/native';
 
-const Splash = () => {
+const WelcomeScreen = () => {
+  const navigation = useNavigation();
+
+  const goToLogin = () => {
+    navigation.navigate('LoginScreen'); // Replace 'LoginScreen' with your login screen name
+  };
+
   return (
-    <View style={style.container}>
-      <Text style={style.txt}>index</Text>
+    <View style={styles.container}>
+      <Video
+        source={require('../../assets/video/pokemon.mp4')} // Replace with your video file path
+        style={StyleSheet.absoluteFill}
+        resizeMode="cover"
+        repeat={true}
+      />
+      <View style={styles.overlay}>
+        {/* Your content over the video */}
+        <Text style={styles.title}>Welcome PokemonXYZ</Text>
+        <TouchableOpacity style={styles.button} onPress={goToLogin}>
+          <Text style={styles.buttonText}>Get Started</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
-export default Splash;
-
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: 'black',
   },
-  txt: {
-    fontSize: 20,
-    color: 'black',
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: 'rgba(0, 0, 0, 0.5)', // Adjust overlay opacity as needed
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'gray',
+    marginBottom: 20,
+  },
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
   },
 });
+
+export default WelcomeScreen;
