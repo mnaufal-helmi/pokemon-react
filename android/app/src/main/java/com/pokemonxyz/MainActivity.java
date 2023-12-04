d6f271731820ea6b02b1499328f1ad8bcf578403
mnaufalhelmi <mnaufalhelmi@gmail.com>
2023-12-04 15:06:20 +0700

    adjustment splashscreen

--- android/app/src/main/java/com/pokemonxyz/MainActivity.java
+++ android/app/src/main/java/com/pokemonxyz/MainActivity.java
@@ -7,6 +7,8 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+// import com.facebook.FacebookSdk;
+// import com.facebook.appevents.AppEventsLogger;
 
 public class MainActivity extends ReactActivity {
     
